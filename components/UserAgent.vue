5400c960abb4bfc5db6ae50ff8ac7bf90bbb788c
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-05-06 10:42:20 -0500

    component for user agent added

---
+++ components/UserAgent.vue
@@ -0,0 +1,35 @@
+<template>
+    <span class="userAgent"></span>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            userbrowser: detectAgent()
+        }
+    }
+}
+
+function detectAgent(){
+    /* "process.client" is to ensure that the navigator is 
+    never referenced during the server side render*/
+    if(process.client){
+        if(navigator.userAgent.indexOf('Firefox') !== -1){
+            return 'Firefox';
+        }
+        else if(navigator.userAgent.indexOf('Chrome') !== -1){
+            return 'Chrome';
+        }
+        else if(navigator.userAgent.indexOf('Chrome') !== -1){
+            return 'Chrome';
+        }
+        else if(navigator.userAgent.indexOf('Safari') !== -1){
+            return 'Safari';
+        }
+        else{
+            return 'Browser';
+        }
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,22 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        myfirstapp
-      </h1>
-      <h2 class="subtitle">
-        My exceptional Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <h1 class="statement">The amazing new Widget is coming soon</h1>
+    <p class="subtext">It's revolutionary due to it will change the world of <UserAgent />  widgets as we know it</p>
+    <p class="subtext">Be sure to sign-up to find out more about the next generation of widgets
+    and follow our progress</p>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import UserAgent from '../components/UserAgent';
 
 export default {
   components: {
-    Logo
+    UserAgent
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
